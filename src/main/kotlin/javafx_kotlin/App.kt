fcb651cf813488ab467b33247a52780282f5ba38
insolor <insolor@gmail.com>
2020-11-27 12:19:00 +0300

    Remove redundant imports

--- src/main/kotlin/javafx_kotlin/App.kt
+++ src/main/kotlin/javafx_kotlin/App.kt
@@ -3,8 +3,6 @@ package javafx_kotlin
 import javafx.application.Application
 import javafx.application.Application.launch
 import javafx.scene.Scene
-import javafx.scene.control.Label
-import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class App : Application() {
